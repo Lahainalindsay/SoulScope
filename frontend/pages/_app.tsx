b3f762849bb2ce20ae173303b3269a5ff0eef799
LindsayCoral <lahainalindsay9111@gmail.com>
2026-07-12 15:20:29 -1000

    Load shared premium typography styles

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "../lib/supabaseClient";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import "../styles/premium-typography.css";
 import "../styles/responsive.css";
 
 export default function App({ Component, pageProps }: AppProps & { pageProps: { initialSession?: any } }) {
